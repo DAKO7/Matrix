d6728db5a8ad72a29b12d03d51699109eeb3f84c
Opilane TTHK <opilane@tthk.ee>
2019-09-10 15:11:18 +0300

    Second upload

--- src/Koroljov/company/Main.java
+++ src/Koroljov/company/Main.java
@@ -1,8 +1,5 @@
 package Koroljov.company;
-
-
-
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
 
@@ -23,16 +20,16 @@ public class Main {
         int[][] nums2 = new int[][]
                 {
                         {9, 8, 7},
-                        {3, 2, 1},
-                        {4, 5, 6}
+                        {4, 5, 6},
+                        {7, 8, 9}
                 };
 
 
         System.out.printf("\n|\t%d\t%d\t%d\t|\t\t|\t%d\t%d\t%d\t|", nums[0][0], nums[0][1], nums[0][2], nums2[0][0], nums2[0][1], nums2[0][2]);
-        System.out.printf("\n|\t%d\t%d\t%d\t|\t+\t|\t%d\t%d\t%d\t|", nums[1][0], nums[1][1], nums[1][2], nums2[1][0], nums2[1][1], nums2[1][2]);
+        System.out.printf("\n|\t%d\t%d\t%d\t|\t?\t|\t%d\t%d\t%d\t|", nums[1][0], nums[1][1], nums[1][2], nums2[1][0], nums2[1][1], nums2[1][2]);
         System.out.printf("\n|\t%d\t%d\t%d\t|\t\t|\t%d\t%d\t%d\t|", nums[2][0], nums[2][1], nums[2][2], nums2[2][0], nums2[2][1], nums2[2][2]);
 
-        System.out.println("\nWhat operation?");
+        System.out.println("\nWhat operation? + - *");
         operator = scan0object.next().charAt(0);
 
         int answer = 0;
@@ -59,15 +56,6 @@ public class Main {
             case '*': for (int i = 0; i < nums.length; i++) {
                 for (int j = 0; j < nums[i].length; j++) {
                     System.out.print(nums[i][j] * nums2[i][j]+"\t");
-                }
-                System.out.println();
-            }
-                break;
-
-            case '/': for (int i = 0; i < nums.length; i++) {
-                for (int j = 0; j < nums[i].length; j++) {
-                    System.out.print(nums[i][j] / nums2[i][j]+"\t");
-                    //System.out.printf("%d", nums[i][j] / nums2[i][j]);
                 }
                 System.out.println();
             }
